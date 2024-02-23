e47c543302da17ea974c6e684c075abd12276d0f
Leonardo Siagian <if319017@students.del.ac.id>
2024-02-23 13:42:51 +0700

    Error Constant

---
+++ src/main/java/com/springboot/sftp/constant/ErrorConstant.java
@@ -0,0 +1,10 @@
+package com.springboot.sftp.constant;
+
+/**
+ * @Author: Leonardo Siagian
+ * Created on 23/02/2024
+ */
+public class ErrorConstant {
+    public static final String DEFAULT_ERROR_CODE = "999999";
+    public static final String DEFAULT_ERROR_MESSAGE = "Kesalahan System";
+}
